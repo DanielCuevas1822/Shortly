829c63337037a9ff891499cb8f2711954d587771
Daniel Cuevas <daniel_cuevas_hernandez@outlook.com>
2021-05-14 12:52:11 -0500

    Create ShortenLInk component styles

--- src/components/ShortenLink.jsx
+++ src/components/ShortenLink.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 
+import "../assets/styles/components/ShortenLink.scss";
+import "../assets/styles/components/Buttons.scss";
+
 const ShortenLink = () => {
   return (
     <section>
       <div className="container">
-        <div className="row">
+        <div
+          className="row shorten"
+          style={{ backgroundImage: "url(/img/bg-shorten-desktop.svg)" }}
+        >
           <div className="col-12">
-            <form action="">
-              <input type="text" name="" id="" />
-              <button type="submit">Shorten it!</button>
+            <form className="shorten__form">
+              <input
+                className="shorten__form__input"
+                type="text"
+                name=""
+                id=""
+                placeholder="Shorten a link here..."
+              />
+              <button className="shorten__form__btn btn--green" type="submit">
+                Shorten it!
+              </button>
             </form>
           </div>
         </div>
