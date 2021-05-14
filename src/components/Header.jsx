b7e6b9d3c7aa9fa354e4d545b6678c6089a996bf
Daniel Cuevas <daniel_cuevas_hernandez@outlook.com>
2021-05-14 12:46:06 -0500

    Create header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,33 @@
 import React from "react";
+import logo from "../assets/img/logo.svg";
+
+import "../assets/styles/components/Header.scss";
+import "../assets/styles/components/Buttons.scss";
 
 const Header = () => {
   return (
     <header>
       <div className="container">
-        <div className="row">
-          <div className="col-3">
-            <img src="" alt="logo" />
+        <div className="row header">
+          <div className="col-md-2 col-sm-12 header__logo">
+            <img src={logo} alt="logo" />
           </div>
-          <div className="col-6">
+          <div className="col-md-6 col-sm-12 header__nav">
             <nav>
               <ul>
-                <li>Features</li>
-                <li>Pricing</li>
-                <li>Resources</li>
+                <li className="header__nav__item">Features</li>
+                <li className="header__nav__item">Pricing</li>
+                <li className="header__nav__item">Resources</li>
               </ul>
             </nav>
           </div>
-          <div className="col-3">
-            <button>Login</button>
-            <button>Sign Up</button>
+          <div className="col-md-4 col-sm-12 header__profile">
+            <button className="header__profile__loginbtn btn--white">
+              Login
+            </button>
+            <button className="header__profile__signupbtn  btn--green">
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
