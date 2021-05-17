175844618511cf12fb7822e3285c3580273149d5
Daniel Cuevas <daniel_cuevas_hernandez@outlook.com>
2021-05-17 15:59:18 -0500

    Use axios

--- src/components/ShortenLink.jsx
+++ src/components/ShortenLink.jsx
@@ -2,18 +2,34 @@ import React, { useState } from "react";
 
 import "../assets/styles/components/ShortenLink.scss";
 import "../assets/styles/components/Buttons.scss";
+import axios from "axios";
+import Loader from "./Loader";
 
 const ShortenLink = () => {
   const [link, setLink] = useState("");
   const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [shorter, setShorter] = useState([]);
+
   const handleChange = (e) => {
     setLink(e.target.value);
     setError(false);
   };
+
   const handleClick = (e) => {
     e.preventDefault();
-
     if (link) {
+      setIsLoading(true);
+      axios
+        .get(`https://api.shrtco.de/v2/shorten?url=${link}`)
+        .then((response) => {
+          setShorter([...shorter, response.data]);
+          setIsLoading(false);
+          setLink("");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     } else {
       setError(true);
     }
@@ -41,47 +57,45 @@ const ShortenLink = () => {
                   Please add link
                 </small>
               ) : null}
-              <button
-                onClick={handleClick}
-                className="shorten__form__btn btn--green"
-                type="submit"
-              >
-                Shorten it!
-              </button>
+              {!isLoading ? (
+                <button
+                  onClick={handleClick}
+                  className="shorten__form__btn btn--green"
+                  type="submit"
+                >
+                  Shorten it!
+                </button>
+              ) : null}
             </form>
           </div>
         </div>
-        <div className="row">
-          <div className="col-12 link">
-            <div className="link__item">
-              <p className="link__item__original">
-                https://www.youtube.com/watch?v=hrGsV80Ke4Q
-              </p>
-              <hr />
-              <p className="link__item__shorted">shrtco.de/FuHBxz</p>
-              <button className="link__item__btn btn--green">Copy</button>
-            </div>
-          </div>
-          <div className="col-12 link">
-            <div className="link__item">
-              <p className="link__item__original">
-                https://www.youtube.com/watch?v=hrGsV80Ke4Q
-              </p>
-              <hr />
-              <p className="link__item__shorted">shrtco.de/FuHBxz</p>
-              <button className="link__item__btn btn--green">Copy</button>
-            </div>
-          </div>
-          <div className="col-12 link">
-            <div className="link__item">
-              <p className="link__item__original">
-                https://www.youtube.com/watch?v=hrGsV80Ke4Q
-              </p>
-              <hr />
-              <p className="link__item__shorted">shrtco.de/FuHBxz</p>
-              <button className="link__item__btn btn--green">Copy</button>
-            </div>
-          </div>
+        <div className="row loader">
+          {shorter.length > 0
+            ? shorter.map((item, index) => (
+                <div key={index} className="col-12 link">
+                  <div className="link__item">
+                    <p className="link__item__original">
+                      {item.result.original_link}
+                    </p>
+                    <hr />
+                    <p className="link__item__shorted">
+                      {item.result.short_link}
+                    </p>
+                    <button
+                      onClick={(e) => {
+                        navigator.clipboard.writeText(item.result.short_link);
+                        e.target.classList.add("copied");
+                        e.target.innerHTML = "Copied!";
+                      }}
+                      className="link__item__btn btn--green"
+                    >
+                      Copy
+                    </button>
+                  </div>
+                </div>
+              ))
+            : null}
+          {isLoading ? <Loader /> : null}
         </div>
       </div>
     </section>
